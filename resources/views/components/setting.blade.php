b420f3eb3c35d7d6274bd80a1e2e4ef4b45c102a
oyenet1 <bowofadeoyerinde@gmail.com>
2023-05-02 18:33:55 +0100

    ok done

---
+++ resources/views/components/setting.blade.php
@@ -0,0 +1,44 @@
+<section x-cloak x-transition:enter="transition duration-300 ease-in-out transform sm:duration-500"
+    x-transition:enter-start="-translate-x-full" x-transition:enter-end="translate-x-0"
+    x-transition:leave="transition duration-300 ease-in-out transform sm:duration-500"
+    x-transition:leave-start="translate-x-0" x-transition:leave-end="-translate-x-full" x-show="isSearchPanelOpen"
+    @keydown.escape="isSearchPanelOpen = false"
+    class="fixed inset-y-0 z-20 w-full max-w-xs bg-white shadow-xl dark:bg-darker dark:text-light sm:max-w-md focus:outline-none">
+    <div class="absolute right-0 p-2 transform translate-x-full">
+        <!-- Close button -->
+        <button @click="isSearchPanelOpen = false" class="p-2 text-white rounded-md focus:outline-none focus:ring">
+            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </button>
+    </div>
+
+    <h2 class="sr-only">Search panel</h2>
+    <!-- Panel content -->
+    <div class="flex flex-col h-screen">
+        <!-- Panel header (Search input) -->
+        <div
+            class="relative flex-shrink-0 px-4 py-8 text-gray-400 border-b dark:border-primary-darker dark:focus-within:text-light focus-within:text-gray-700">
+            <span class="absolute inset-y-0 inline-flex items-center px-4">
+                <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+            </span>
+            <input x-ref="searchInput" type="text"
+                class="w-full py-2 pl-10 pr-4 border rounded-full dark:bg-dark dark:border-transparent dark:text-light focus:outline-none focus:ring"
+                placeholder="Search..." />
+        </div>
+
+        <!-- Panel content (Search result) -->
+        <div class="flex-1 px-4 pb-4 space-y-4 overflow-y-hidden h hover:overflow-y-auto">
+            <h3 class="py-2 text-sm font-semibold text-gray-600 dark:text-light">History</h3>
+            <p class="px=4">Search resault</p>
+            <!--  -->
+            <!-- Search content -->
+            <!--  -->
+        </div>
+    </div>
+</section>
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -401,7 +401,7 @@
                   </button>
 
                   <!-- User dropdown menu -->
-                  <div x-show="open" x-ref="userMenu" x-transition:enter="transition-all transform ease-out"
+                  <div x-cloak x-show="open" x-ref="userMenu" x-transition:enter="transition-all transform ease-out"
                     x-transition:enter-start="translate-y-1/2 opacity-0"
                     x-transition:enter-end="translate-y-0 opacity-100"
                     x-transition:leave="transition-all transform ease-in"
@@ -779,7 +779,8 @@
           <!-- Main content -->
           <div class="flex items-center justify-center flex-1 h-full p-4">
             <main>
-              <h1 class="text-3xl font-semibold">Page content</h1>
+              {{ $slot ?? '' }}
+              @yield('contents')
             </main>
           </div>
         </div>
@@ -794,7 +795,7 @@
           @click="isSettingsPanelOpen = false" class="fixed inset-0 z-10 bg-primary-darker" style="opacity: 0.5"
           aria-hidden="true"></div>
         <!-- Panel -->
-        <section x-transition:enter="transition duration-300 ease-in-out transform sm:duration-500"
+        <section x-cloak x-transition:enter="transition duration-300 ease-in-out transform sm:duration-500"
           x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0"
           x-transition:leave="transition duration-300 ease-in-out transform sm:duration-500"
           x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full" x-ref="settingsPanel"
@@ -865,17 +866,17 @@
               <div class="p-4 space-y-4 md:p-8">
                 <h6 class="text-lg font-medium text-gray-400 dark:text-light">Colors</h6>
                 <div>
-                  <button @click="setColors('cyan')" class="w-10 h-10 rounded-full"
+                  <button @click="setColors('cyan')" class="w-8 h-8 rounded-full"
                     style="background-color: var(--color-cyan)"></button>
-                  <button @click="setColors('teal')" class="w-10 h-10 rounded-full"
+                  <button @click="setColors('teal')" class="w-8 h-8 rounded-full"
                     style="background-color: var(--color-teal)"></button>
-                  <button @click="setColors('green')" class="w-10 h-10 rounded-full"
+                  <button @click="setColors('green')" class="w-8 h-8 rounded-full"
                     style="background-color: var(--color-green)"></button>
-                  <button @click="setColors('fuchsia')" class="w-10 h-10 rounded-full"
+                  <button @click="setColors('fuchsia')" class="w-8 h-8 rounded-full"
                     style="background-color: var(--color-fuchsia)"></button>
-                  <button @click="setColors('blue')" class="w-10 h-10 rounded-full"
+                  <button @click="setColors('blue')" class="w-8 h-8 rounded-full"
                     style="background-color: var(--color-blue)"></button>
-                  <button @click="setColors('violet')" class="w-10 h-10 rounded-full"
+                  <button @click="setColors('violet')" class="w-8 h-8 rounded-full"
                     style="background-color: var(--color-violet)"></button>
                 </div>
               </div>
@@ -891,7 +892,7 @@
           @click="isNotificationsPanelOpen = false" class="fixed inset-0 z-10 bg-primary-darker" style="opacity: 0.5"
           aria-hidden="true"></div>
         <!-- Panel -->
-        <section x-transition:enter="transition duration-300 ease-in-out transform sm:duration-500"
+        <section x-cloak x-transition:enter="transition duration-300 ease-in-out transform sm:duration-500"
           x-transition:enter-start="-translate-x-full" x-transition:enter-end="translate-x-0"
           x-transition:leave="transition duration-300 ease-in-out transform sm:duration-500"
           x-transition:leave-start="translate-x-0" x-transition:leave-end="-translate-x-full" x-ref="notificationsPanel"
@@ -957,50 +958,7 @@
           @click="isSearchPanelOpen = false" class="fixed inset-0 z-10 bg-primary-darker" style="opacity: 0.5"
           aria-hidden="ture"></div>
         <!-- Panel -->
-        <section x-transition:enter="transition duration-300 ease-in-out transform sm:duration-500"
-          x-transition:enter-start="-translate-x-full" x-transition:enter-end="translate-x-0"
-          x-transition:leave="transition duration-300 ease-in-out transform sm:duration-500"
-          x-transition:leave-start="translate-x-0" x-transition:leave-end="-translate-x-full" x-show="isSearchPanelOpen"
-          @keydown.escape="isSearchPanelOpen = false"
-          class="fixed inset-y-0 z-20 w-full max-w-xs bg-white shadow-xl dark:bg-darker dark:text-light sm:max-w-md focus:outline-none">
-          <div class="absolute right-0 p-2 transform translate-x-full">
-            <!-- Close button -->
-            <button @click="isSearchPanelOpen = false" class="p-2 text-white rounded-md focus:outline-none focus:ring">
-              <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
-          </div>
-
-          <h2 class="sr-only">Search panel</h2>
-          <!-- Panel content -->
-          <div class="flex flex-col h-screen">
-            <!-- Panel header (Search input) -->
-            <div
-              class="relative flex-shrink-0 px-4 py-8 text-gray-400 border-b dark:border-primary-darker dark:focus-within:text-light focus-within:text-gray-700">
-              <span class="absolute inset-y-0 inline-flex items-center px-4">
-                <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-              </span>
-              <input x-ref="searchInput" type="text"
-                class="w-full py-2 pl-10 pr-4 border rounded-full dark:bg-dark dark:border-transparent dark:text-light focus:outline-none focus:ring"
-                placeholder="Search..." />
-            </div>
-
-            <!-- Panel content (Search result) -->
-            <div class="flex-1 px-4 pb-4 space-y-4 overflow-y-hidden h hover:overflow-y-auto">
-              <h3 class="py-2 text-sm font-semibold text-gray-600 dark:text-light">History</h3>
-              <p class="px=4">Search resault</p>
-              <!--  -->
-              <!-- Search content -->
-              <!--  -->
-            </div>
-          </div>
-        </section>
+        <x-setting />
       </div>
     </div>
     <!-- All javascript code in this project for now is just for demo DON'T RELY ON IT  -->
